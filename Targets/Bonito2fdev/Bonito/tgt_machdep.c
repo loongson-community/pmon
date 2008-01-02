748ba41dd9a86a79eece8975804342732a9722e2
cpu <cpu@214b0138-1524-0410-9122-e5cb4b5bc56c>
2008-01-02 23:47:06 +0000

    r327@Knoppix:  root | 2008-01-03 07:46:37 +0800  update 2feva.
    git-svn-id: file:///svn/pmon-all/pmon-all@170 214b0138-1524-0410-9122-e5cb4b5bc56c

--- Targets/Bonito2fdev/Bonito/tgt_machdep.c
+++ Targets/Bonito2fdev/Bonito/tgt_machdep.c
@@ -189,11 +189,13 @@ void
 initmips(unsigned int memsz)
 {
 
+#ifdef DEVBD2F_SM502
 {
 /*set lio bus to 16 bit*/
 volatile int *p=0xbfe00108;
 *p=((*p)&~(0x1f<<8))|(0x8<<8) |(1<<13);
 }
+#endif
 
 /*enable float*/
 memsz=512;
