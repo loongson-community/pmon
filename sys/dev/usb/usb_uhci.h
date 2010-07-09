5e3c97b18cea8557b29fde61c993e91557c708be
LI Wengang <liwengang@loongson.cn>
2010-07-09 16:01:20 +0800

    USB works correctly
    USB work correctly.
    
    TARGET: Bonito2g690e

--- sys/dev/usb/usb_uhci.h
+++ sys/dev/usb/usb_uhci.h
@@ -217,17 +217,17 @@ struct uhci {
 	//struct uhci_qh *skelqh[UHCI_NUM_SKELQH];	/* Skeleton QH's */
 
 	int is_suspended;
-uhci_td_t td_int0 __attribute__((aligned(128),section(".bss.align128")));
-uhci_td_t td_int[8] __attribute__((aligned(128),section(".bss.align128")));/* Interrupt Transfer descriptors */
-uhci_qh_t qh_cntrl __attribute__((aligned(128),section(".bss.align128")));/* control Queue Head */
-uhci_qh_t qh_bulk __attribute__((aligned(128),section(".bss.align128")));/*  bulk Queue Head */
-uhci_qh_t qh_end __attribute__((aligned(128),section(".bss.align128")));/* end Queue Head */
-uhci_td_t td_last __attribute__((aligned(128),section(".bss.align128"))); /* last TD (linked with end chain) */
-unsigned char hc_setup[32] __attribute__((aligned(128),section(".bss.align128")));
-unsigned char control_buf0[256] __attribute__((aligned(128),section(".bss.align128")));
-uhci_td_t tmp_td[USB_MAX_TEMP_TD] __attribute__((aligned(128),section(".bss.align128")));/* temporary bulk/control td's  */
-uhci_td_t tmp_int_td[USB_MAX_TEMP_INT_TD] __attribute__((aligned(128),section(".bss.align128"))); /* temporary interrupt td's  */
-unsigned long framelist[1024] __attribute__ ((aligned (0x1000),section(".bss.align4096"))); /* frame list */
+	uhci_td_t td_int0;
+	uhci_td_t td_int[8];/* Interrupt Transfer descriptors */
+	uhci_qh_t qh_cntrl;/* control Queue Head */
+	uhci_qh_t qh_bulk;/*  bulk Queue Head */
+	uhci_qh_t qh_end;/* end Queue Head */
+	uhci_td_t td_last; /* last TD (linked with end chain) */
+	unsigned char hc_setup[32];
+	unsigned char control_buf0[256];
+	uhci_td_t tmp_td[USB_MAX_TEMP_TD];/* temporary bulk/control td's  */
+	uhci_td_t tmp_int_td[USB_MAX_TEMP_INT_TD]; /* temporary interrupt td's  */
+	unsigned long framelist[1024]; /* frame list */
 };
 typedef struct uhci uhci_t;
 #define td_int uhci->td_int
