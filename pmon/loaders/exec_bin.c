1354cf0eea1eb6ffd6f14631e3050363d9a2f3b5
qianyl <qianyl@debian.(none)>
2010-09-13 10:27:16 +0800

    Speed up the initrd
    1.Enlarge the size of IO read(from 512 to 4096*16*16 bytes) at line
      54 of pmon/loaders/exec_bin.c to speed up the initrd

--- pmon/loaders/exec_bin.c
+++ pmon/loaders/exec_bin.c
@@ -50,7 +50,8 @@ static long
    load_bin (int fd, char *buf, int *n, int flags)
 {
 	void *addr = (void *)dl_offset;
-	int size = 512;
+	//int size = 512;
+	int size = 4096*16*16;
 	int n2;
 	int count = 0;
 
