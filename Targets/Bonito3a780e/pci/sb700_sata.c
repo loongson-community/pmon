91b6571f55d65f2b4f38168b072babfeabedc5ff
WANG Yulong <wangyulong@loongson.cn>
2010-12-23 16:16:18 +0800

    Adjust sata PHY port1 configuration to pass the SI testing.
    Adjust the TX main driving swing and pre-emphasis driver swing to pass
    the sata SI testing.
    
    Target: Bonito3a780e

--- Targets/Bonito3a780e/pci/sb700_sata.c
+++ Targets/Bonito3a780e/pci/sb700_sata.c
@@ -179,7 +179,7 @@ static void sata_init(device_t dev)
 	/* RPR7.6.2 SATA GENI PHY ports setting */
 	printk_info("sata geni phy ports setting\n");
 	pci_write_config32(dev, 0x88, 0x01B48017);
-	pci_write_config32(dev, 0x8c, 0x01B48019);
+	pci_write_config32(dev, 0x8c, 0x01B48016);
 	pci_write_config32(dev, 0x90, 0x01B48016);
 	pci_write_config32(dev, 0x94, 0x01B48016);
 	pci_write_config32(dev, 0x98, 0x01B48016);
@@ -188,7 +188,7 @@ static void sata_init(device_t dev)
 	/* RPR7.6.3 SATA GEN II PHY port setting for port [0~5]. */
 	printk_info("sata gen II PHY port setting for port\n");
 	pci_write_config16(dev, 0xA0, 0xA09A);
-	pci_write_config16(dev, 0xA2, 0xA09F);
+	pci_write_config16(dev, 0xA2, 0xA09A);
 	pci_write_config16(dev, 0xA4, 0xA07A);
 	pci_write_config16(dev, 0xA6, 0xA07A);
 	pci_write_config16(dev, 0xA8, 0xA07A);
