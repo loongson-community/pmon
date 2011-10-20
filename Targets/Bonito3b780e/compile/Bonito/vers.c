e328989fb05127ca67310860b2e35a937b07e4f3
sizhiying <sizhiying@loongson.cn>
2011-10-20 11:06:11 +0800

    Add Targets/Bonito3b780e into pmon3a.  Make softlink Bonito3b780e/pci to Bonito3a780e/pci.  For this, 3A and 3B can use the same pci configure.
    Signed-off-by: mengxiaofu <mengxiaofu@ict.ac.cn>

---
+++ Targets/Bonito3b780e/compile/Bonito/vers.c
@@ -0,0 +1,6 @@
+char ostype[] = "PMON2011";
+char osrelease[] = "2.1";
+char osversion[] = "Bonito3b780e#522";
+char sccs[8] = { ' ', ' ', ' ', ' ', '@', '(', '#', ')' };
+char vers[] =
+    "PMON2011 2.1 (Bonito3b780e) #522: Wed Oct 19 10:56:40 CST 2011 commit be326cd02be688e3e3f8fab78e8094f96c199306 Author: xueshuangbai@ict.ac.cn <xiaqichao@ict.ac.cn> Date:   Sat Oct 15 16:11:56 2011 +0800 ";
---
+++ Targets/Bonito3b780e/compile/Bonito/vers.h
@@ -0,0 +1,1 @@
+#define VERS "PMON2011 2.1 (Bonito3b780e) #522: Wed Oct 19 10:56:40 CST 2011\r\n"
