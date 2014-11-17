f2338ad92e70df0fffc269a2acae0214d2d3121b
Huang Pei <huangpei@loongson.cn>
2014-11-17 09:54:03 +0800

    [3aserver]:porting 508a34afa472c00ef8bfd694d497e8024509eeda to 3aserver
    Change-Id: I0e4efee507d49b342b6e3f22ecbb11fc7faf9994

--- Targets/Bonito3aserver/Bonito/tgt_machdep.c
+++ Targets/Bonito3aserver/Bonito/tgt_machdep.c
@@ -1788,17 +1788,11 @@ tgt_machprint()
  *  Usually top of RAM memory.
  */
 
-	register_t
+register_t
 tgt_clienttos()
 {
-	return((register_t)(int)PHYS_TO_CACHED(memorysize & ~7) - 64);
-	/*
-#if LS3_HT
-return((register_t)(int)PHYS_TO_CACHED(memorysize & ~7) - 64);
-#else    
-return((register_t)(int)PHYS_TO_UNCACHED(memorysize & ~7) - 64);
-#endif    
-	 */
+	extern char start[];
+	return(register_t)(int)PHYS_TO_CACHED(start - 64);
 }
 
 #ifdef HAVE_FLASH
