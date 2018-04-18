6f626bd651ee7bd5c084918bfb667c556f9a2f6d
QiaoChong <qiaochong@loongson.cn>
2018-04-18 15:08:09 +0800

    fix ls2k pci mem addr convert for dc.
    Change-Id: Ic4c4cbc0e97a0ddb0d410822c2cb6cd8da5d9e5d
    Signed-off-by: QiaoChong <qiaochong@loongson.cn>

--- Targets/LS2K/ls2k/dc.c
+++ Targets/LS2K/ls2k/dc.c
@@ -3,7 +3,12 @@
 #include <stdio.h>
 #include <sys/malloc.h>
 #include <machine/pio.h>
+#include <sys/device.h>
 #include <target/ls2k.h>
+#include <dev/pci/pcireg.h>
+#include <dev/pci/pcivar.h>
+#include <dev/pci/pcidevs.h>
+#include <pmon/dev/pcibrvar.h>
 
 #define DC_FB0 1	//mtf modify
 
@@ -310,7 +315,7 @@ int dc_init()
 	val = pci_read_type0_config32(6, 0, 16);
 
 	val &= 0xffff0000;
-	val |= 0x80000000;
+	val |= _pci_bus[0]->pa.pa_memt->bus_base;
 	printf("val %x\n", val);
 	config_fb(val + DC0_BASE_ADDR_OFF);	//for dvo_0 1240
 	config_fb(val + DC1_BASE_ADDR_OFF);	//for dvo_1 1250
