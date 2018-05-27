18d861b75dcd8dc5cb814ec742861b98853e02a5
QiaoChong <qiaochong@loongson.cn>
2018-05-27 11:00:40 +0800

    update 3a9780e lvds config, use LVDS macro to select 18 and 24bit vgarom.
    Change-Id: I6d5444a3ea975a95ec4b1f5c4976f2dfa2ed81d9
    Signed-off-by: QiaoChong <qiaochong@loongson.cn>

--- Targets/Bonito3a9780e/Bonito/tgt_machdep.c
+++ Targets/Bonito3a9780e/Bonito/tgt_machdep.c
@@ -141,7 +141,13 @@ extern const char *kbd_error_msgs[];
 int vga_available=0;
 #elif defined(VGAROM_IN_BIOS)
 #ifdef LVDS
+#if LVDS===18
+#include "Targets/Bonito3a780e/Bonito/vgarom-780e-lvds-sl-18bit.h"
+#elif LVDS===24
+#include "Targets/Bonito3a780e/Bonito/vgarom-780-lvds-dl-24bit.h"
+#else
 #include "Targets/Bonito3a780e/Bonito/vgarom-lvds.c"
+#endif
 #else
 #include "vgarom.c"
 #endif
